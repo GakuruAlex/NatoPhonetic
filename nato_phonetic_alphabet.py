75225388caf47f7f0010ee37c1f5fc6417c1efa2
alexgicimo <gakurualexgicimo@gmail.com>
2024-09-10 16:34:40 +0300

    read the phonetic csv and return a dictionary for the letter and code

---
+++ nato_phonetic_alphabet.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+class NatoPhonetic:
+    def nato_phonetic(self)-> dict:
+        """_Read the csv file and process the data into a dictionary of letter as key and code as value_
+
+        Returns:
+            dict: _A dictionary of letter code pair _
+        """
+        phonetic_data = pd.read_csv("nato_phonetic_alphabet.csv")
+
+        phonetic_data_dict = {row[1]["letter"]: row[1]["code"] for row in phonetic_data.iterrows()}
+        print(phonetic_data_dict)
+
+        return phonetic_data_dict
+    def code_given_word(self, word: str) -> list:
+        """_Given a word , process and return equivalent phonetics for that word_
+
+        Args:
+            word (str): _Given word_
+
+        Returns:
+            list: _A list of the phonetic equivalent of given word_
+        """
+        data = self.nato_phonetic()
+        coded = [data[letter] for letter in word]
+        return coded
